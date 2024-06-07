0eb8bca6c10fe952ff97159aec5097dff34aa12d
Yohan Valencia <yohan.valencia@holded.com>
2024-06-07 16:44:57 +0200

    Add is_blacklisted

---
+++ holded-dataflow/filter.py
@@ -0,0 +1,13 @@
+def is_blacklisted(name: str, filter_type: str) -> bool:
+    """
+    Check if an event is blacklisted.
+
+    Parameters:
+    - name: Name of the event to check.
+    - filter_type: Type of the filter to apply. Valid values are "PIPELINE" or "SSGTM".
+
+    Returns:
+    - True if the event is blacklisted for the specified filter type, False otherwise.
+    """
+    # Function implementation remains the same
+    pass
